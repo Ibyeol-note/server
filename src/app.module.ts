e6bc2b8a903e11887810d7101a248cf4a554156f
KKardd <wlstjd4013@gmail.com>
2024-10-09 23:05:08 +0900

    feat: DI graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserResolver } from './user/user.resolver';
 import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 자동으로 schema.gql 파일 생성
+      playground: true, // GraphQL Playground 활성화
+    }),
+    UserModule,
+  ],
   providers: [AppService, UserResolver],
 })
 export class AppModule {}
