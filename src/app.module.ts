0a2c4efe6e4e6b7ab06c081c4f23ce5ec1e86599
KKardd <wlstjd4013@gmail.com>
2024-10-18 22:46:39 +0900

    refactor: modify schema-first method

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 자동으로 schema.gql 파일 생성
+      typePaths: ['./**/*.gql'],
+      debug: true,
       playground: true, // GraphQL Playground 활성화
     }),
     UserModule,
