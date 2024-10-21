82ad1ff7df85627f9c776d27d9a5d7fc7bb9d6c8
KKardd <wlstjd4013@gmail.com>
2024-10-21 23:53:39 +0900

    refactor: modify app module setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserResolver } from './user/user.resolver';
 import { UserModule } from './user/user.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      typePaths: ['./**/*.gql'],
-      debug: true,
-      playground: true, // GraphQL Playground 활성화
-    }),
-    UserModule,
-  ],
-  providers: [AppService, UserResolver],
+  imports: [UserModule],
+  providers: [AppService],
 })
 export class AppModule {}
