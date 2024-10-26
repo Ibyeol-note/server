8e7b8fd70538428d4b8cca005d630f257f6cb6b1
KKardd <wlstjd4013@gmail.com>
2024-10-26 15:56:32 +0900

    feat: DI modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PostModule, AuthModule],
   providers: [AppService],
 })
 export class AppModule {}
