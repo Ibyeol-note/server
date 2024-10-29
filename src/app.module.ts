76d17a300de50bc13db95144f1bd99159baf5dbb
KKardd <wlstjd4013@gmail.com>
2024-10-30 03:00:50 +0900

    fix: DI auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
-import { AuthModule } from './auth/auth.module';
+
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [UserModule, PostModule],
   providers: [AppService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
