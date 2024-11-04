09c5a1a5d060056ccbbff887463320549b84b85b
KKardd <wlstjd4013@gmail.com>
2024-11-04 19:31:42 +0900

    feat: DI auth implements

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthReader } from './implement/auth.reader';
 
 @Module({
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, AuthReader],
+  exports: [AuthService, , AuthReader],
 })
 export class AuthModule {}
