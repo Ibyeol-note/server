c596178d61b5ebf072fd4ef321233cf06932d4d5
KKardd <wlstjd4013@gmail.com>
2024-11-06 18:46:00 +0900

    feat: apply swagger in app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +14,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerDocument = JSON.parse(
+    readFileSync(join(process.cwd(), 'openapi.json'), 'utf8'),
+  );
+
+  // Swagger UI 설정
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   await app.listen(3000);
 }
