fc948c2cb2b374a60d56950fb7d87c6a3947fc1b
KKardd <wlstjd4013@gmail.com>
2024-11-22 00:26:38 +0900

    feat: apply app-controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UserModule, PostModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
