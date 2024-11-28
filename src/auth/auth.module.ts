377bc41164c1316c0da88132b9cf8076dc77487d
KKardd <wlstjd4013@gmail.com>
2024-11-28 21:43:46 +0900

    feat: DI prisma service in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthReader } from './implement/auth.reader';
 import { HttpModule } from '@nestjs/axios';
 import { AuthController } from './auth.controller';
 import { AuthValidator } from './implement/auth.validator';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [HttpModule],
-  providers: [AuthService, AuthReader, AuthValidator],
+  providers: [AuthService, AuthReader, AuthValidator, PrismaService],
   exports: [AuthService, AuthReader, AuthValidator],
   controllers: [AuthController],
 })
