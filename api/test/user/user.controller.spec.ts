4c1b515229799f5eccf45645a22a022b9edaccdd
KKardd <wlstjd4013@gmail.com>
2024-12-04 17:21:23 +0900

    test: modify path in tests

--- api/test/user/user.controller.spec.ts
+++ api/test/user/user.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserController } from '../../api/src/user/user.controller';
-import { UserService } from '../../api/src/user/user.service';
+import { UserController } from '../../src/user/user.controller';
+import { UserService } from '../../src/user/user.service';
 
 describe('UserController', () => {
   let controller: UserController;
