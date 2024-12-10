018ca7c06d93807cb3f9cbed021c041e4fb81e53
KKardd <wlstjd4013@gmail.com>
2024-12-10 21:00:32 +0900

    feat: add check-disappear-by-id about post \implement

--- api/src/post/implement/post.validator.ts
+++ api/src/post/implement/post.validator.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PostException } from 'api/src/global/exceptions/post-exceptions';
+import { PrismaService } from 'api/src/prisma/prisma.service';
 
 @Injectable()
 export class PostValidator {
-  constructor() {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async checkDisappearById(postId: string) {
+    const isExist = await this.prismaService.post.findFirst({
+      where: { id: postId },
+    });
+    if (isExist) {
+      throw PostException.FOUND;
+    }
+  }
 }
