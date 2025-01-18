22a2ae2e71710b604db5bd1c8551d5daf3afdfed
KKardd <wlstjd4013@gmail.com>
2025-01-18 23:20:00 +0900

    fix: healthcheck service routing

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('healthcheck')
+  @Get('/healthcheck')
   @HttpCode(200)
   healthCheck(): string {
     return this.appService.healthCheck();
